7045a9444e9e33b5e1497ce2c529ed1a2c7e3412
fatmaboodai <76260193+fatmaboodai@users.noreply.github.com>
2021-01-03 22:44:08 +0300

    6

--- question1/question1.js
+++ question1/question1.js
@@ -1,1 +1,18 @@
 // 👇🏻 قم بحل السؤال الأول هنا
+function sayHello(name) {
+
+
+
+if(name!= null)
+
+  {console.log(`مرحبا، ${name}`)}
+
+else
+
+{console.log(`error`)}
+}
+
+sayHello(`fatma`);
+sayHello()
+
+
